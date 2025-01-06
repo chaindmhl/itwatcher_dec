85f949e627eae74517b9db0403f8c67743e3f2d2
chaindmhl <117880660+chaindmhl@users.noreply.github.com>
2025-01-06 14:05:32 +0800

    Update admin.py

--- admin.py
+++ admin.py
@@ -1,18 +1,17 @@
 from django.contrib import admin
 # from .models import Video, PlateLog, CountLog, VehicleLog, ColorLog, NVRVideo, OutputVideo, SwervingVideo, BlockingVideo, LPRVideo, ColorVideo, ViolationLog
-from .models import Video, PlateLog, CountLog, VehicleLog, ColorLog, NVRVideo, ViolationLog
+from .models import Video, PlateLog, CountLog, VehicleLog, ColorLog, ViolationLog
 
 # Register your models here.
 admin.site.register(Video)
 admin.site.register(CountLog)
 admin.site.register(VehicleLog)
 admin.site.register(ColorLog)
-admin.site.register(NVRVideo)
 
 class PlateLogAdmin(admin.ModelAdmin):
     list_display = ('timestamp', 'video_file','display_frame_image', 'display_plate_image','plate_number' )  # Include 'display_plate_image' here
 admin.site.register(PlateLog, PlateLogAdmin)
 
 class ViolationLogAdmin(admin.ModelAdmin):
-    list_display = ('timestamp', 'video_file','display_vehicle_image', 'vehicle_type','vehicle_color', 'display_plate_image', 'plate_number', 'display_frame_image',  'violation' )  # Include 'display_plate_image' here
+    list_display = ('timestamp_date', 'timestamp_time','video_file','display_vehicle_image', 'vehicle_type','vehicle_color', 'display_plate_image', 'plate_number', 'display_frame_image',  'violation' )  # Include 'display_plate_image' here
 admin.site.register(ViolationLog, ViolationLogAdmin)
